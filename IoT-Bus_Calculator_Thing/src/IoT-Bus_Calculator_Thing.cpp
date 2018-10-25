a73ae49248f93fff99c7bf549173a5774212d68a
ianarchbell <ian@Ians-iMac-8.local>
2018-10-25 06:44:32 -0700

    Added Calculator example

---
+++ IoT-Bus_Calculator_Thing/src/IoT-Bus_Calculator_Thing.cpp
@@ -0,0 +1,369 @@
+
+/*______Import Libraries_______*/
+#include "TFT_eSPI.h" // Hardware-specific library
+#include "colors.h" // extended colors
+#include <XPT2046_Touchscreen.h>
+#include <FS.h>
+#include <WiFi.h>
+/* 
+ *  MOZ IoT 
+ */
+#include <Thing.h>
+#include <WebThingAdapter.h>
+/*______End of Libraries_______*/
+
+// simple window style class used for the response box and buttons
+enum aligment { LEFT, RIGHT, MIDDLE };
+
+class Button{
+public:
+  Button(TFT_eSPI& _tft): tft(_tft){}
+  void setRect(uint16_t _x, uint16_t _y, uint16_t _width, uint16_t _height){
+    x = _x;
+    y = _y;
+    width = _width;
+    height =_height;
+  }
+  void setText(String _text, uint16_t _color){text = _text;color = _color;}
+  String& getText(){return text;}
+  void setType(uint8_t _type){type = _type;}
+  uint8_t getType(){return type;}
+  void setBgColor(uint16_t _bgColor){bgColor = _bgColor;}
+  void setTextAlign(uint8_t _alignment){alignment = _alignment;}
+  void draw(){
+    tft.fillRect(x, y, width, height, bgColor);
+    switch(alignment){
+      default:
+      case MIDDLE:
+        tft.setCursor(x+width/3, y+height/3); // center text
+        break;
+      case LEFT:
+        tft.setCursor(x+10, y+height/3);
+        break;
+      case RIGHT:
+        tft.setCursor(x+width, y+height/3);
+        break;     
+    }
+    // draw the outline
+    tft.drawFastHLine(x, y, width, TFT_WHITE);
+    tft.fillRect(x, y+height-5, width, 5, TFT_DARKGREY);
+    tft.drawFastHLine(x, y+height-1, width, TFT_WHITE);
+    tft.drawFastVLine(x, y, height, TFT_WHITE);
+    tft.fillRect(x+width-5, y, 5, height, TFT_DARKGREY);
+    tft.drawFastVLine(x+width-1, y, height, TFT_WHITE);
+    
+    // draw the text
+    tft.setTextSize(4);
+    tft.setTextColor(color);
+    tft.println(text); 
+  }
+  bool hit(TS_Point p){
+    if (p.x<x+width && p.x>x){
+        if (p.y<y+height && p.y>y){
+          // draw the outline as pushed
+          tft.drawFastHLine(x, y, width, TFT_WHITE);
+          tft.fillRect(x, y, width, 5, TFT_DARKGREY);
+          tft.drawFastHLine(x, y+height-1, width, TFT_WHITE);
+          tft.drawFastVLine(x, y, height, TFT_WHITE);
+          tft.fillRect(x, y, 5, height, TFT_DARKGREY);
+          tft.drawFastVLine(x+width-1, y, height, TFT_WHITE);
+          delay(300); // enough time to be visible
+          draw(); // redraw
+          return true;
+        }
+    }
+    return false;
+  }
+
+private:
+  TFT_eSPI& tft;
+	uint16_t x = 0, y = 0, width = 240, height = 320, color = TFT_WHITE, bgColor = TFT_BLACK;
+  uint8_t type; // user type
+  uint8_t alignment = MIDDLE;
+  String text = "";
+};
+
+/*____Calibration values TFT TS_____*/
+#define TS_MINX 256
+#define TS_MINY 274
+#define TS_MAXX 3632
+#define TS_MAXY 3579
+/*______End of Calibration______*/
+
+class Calculator{
+  private:
+    TFT_eSPI& tft;
+    XPT2046_Touchscreen ts;
+
+    String symbol[4][4] = {
+      { "7", "8", "9", "/" },
+      { "4", "5", "6", "*" },
+      { "1", "2", "3", "-" },
+      { "C", "0", "=", "+" }
+    };
+
+    unsigned int buttonColors[4][4] = {
+      { TFT_LIGHTGREY, TFT_LIGHTGREY, TFT_LIGHTGREY, TFT_LIGHTBLUE },
+      { TFT_LIGHTGREY, TFT_LIGHTGREY, TFT_LIGHTGREY, TFT_LIGHTBLUE },
+      { TFT_LIGHTGREY, TFT_LIGHTGREY, TFT_LIGHTGREY, TFT_LIGHTBLUE },
+      { TFT_LIGHTSALMON, TFT_LIGHTGREY, TFT_LIGHTGREEN, TFT_LIGHTBLUE }
+    };
+
+    enum buttonType{ NUM, CLEAR, EQUALS, DIVIDE, MULTIPLY, ADD, SUBTRACT };
+
+    uint8_t buttonTypes[4][4] = {
+      { NUM, NUM, NUM, DIVIDE },
+      { NUM, NUM, NUM, MULTIPLY },
+      { NUM, NUM, NUM, SUBTRACT },
+      { CLEAR,  NUM, EQUALS, ADD }
+    };
+
+    #define BUTTONCOUNT 16
+    Button* buttons[BUTTONCOUNT];
+
+    long Num1,Num2,Accumulator;
+    char action;
+
+    Button* resultBox;
+    String function = "";
+  
+  public:
+    Calculator(TFT_eSPI& _tft, XPT2046_Touchscreen _ts): tft(_tft), ts(_ts){
+      // initialize display
+      tft.init(); 
+      tft.setRotation(0); 
+      ts.setRotation(0);
+      ts.setCalibration(TS_MINX, TS_MAXX, TS_MINY, TS_MAXY);
+    }
+
+    long getAccumulator(){return Accumulator;}
+    long getNum1(){return Num1;}
+    long getNum2(){return Num2;}
+    String getFunction(){return function;}
+
+    // wait for a button press
+    TS_Point waitTouch() {
+      while (!ts.touched()){
+          // do nothing
+      }
+      Serial.println("touched");
+        // Read the current X and Y axis as co-ordinates at the last touch time
+        // The values were captured when Pressed() was called!
+      TS_Point p = ts.getPoint();
+      Serial.println("--------------------------------");
+      // print raw data
+      Serial.print("raw:");Serial.print(p.x);Serial.print(",");Serial.println(p.y);
+      p = ts.getMappedPoint();
+      // print data mapped to width and height of TFT depending on rotation
+      Serial.print("mapped by library:");Serial.print(p.x);Serial.print(",");Serial.println(p.y);
+      Serial.println("--------------------------------");  
+      return p;
+    }
+
+    void draw()
+    {
+      #define resultBoxHeight 80
+      #define buttonHeight 60
+      #define buttonWidth 60
+
+      resultBox = new Button(tft);
+    
+      // we are using the touchscreen to getwidth as we know it changes with orientation - need to check tft
+      resultBox->setRect(0, 0, ts.getWidth(), resultBoxHeight);
+      resultBox->setBgColor(TFT_BLACK);
+      resultBox->setText(String("IoT-Bus"), TFT_WHITE);
+      resultBox->setTextAlign(LEFT);
+      resultBox->draw();
+
+      // Create keypad
+      for (int j=0;j<4;j++) {
+        for (int i=0;i<4;i++) {
+          buttons[(j*4)+i] = new Button(tft);
+          buttons[(j*4)+i]->setRect( 60*i, resultBoxHeight + (60*j), buttonWidth, buttonHeight );
+          buttons[(j*4)+i]->setBgColor(buttonColors[j][i]);
+          buttons[(j*4)+i]->setText(String(symbol[j][i]), TFT_BLACK);
+          buttons[(j*4)+i]->setType(buttonTypes[j][i]);  
+          buttons[(j*4)+i]->draw();
+        }
+      }
+    }
+
+    // Handle each button
+    void handleButton(Button* button){
+      uint8_t type = button->getType();
+      Serial.println(type);
+      switch(type){
+        default:
+        case NUM:
+          handleNumber(button);
+          displayResult();
+          Serial.println("number");
+          break;
+        case CLEAR:
+          Serial.println("clear");
+          Accumulator=Num1=Num2=0;
+          displayResult(); 
+          break;
+        case EQUALS:
+          handleEquals(button);
+          displayResult();
+          Serial.println("=");
+          break;
+        case MULTIPLY:
+        case DIVIDE:
+        case ADD:
+        case SUBTRACT:
+          action = button->getType();
+          Serial.println("function");
+          Num1 = Accumulator;    
+          Accumulator = 0;
+          function = button->getText();
+          break;
+      }
+    }
+    // Hittest for all buttons
+    Button* hitTest(TS_Point p){
+      for(int i=0;i<BUTTONCOUNT;i++){
+        if(buttons[i]->hit(p)){
+          Serial.println(buttons[i]->getText());
+          handleButton(buttons[i]);
+          return buttons[i];
+        }
+      }
+      return nullptr;
+    }
+
+    void displayResult(){
+    resultBox->setText(String(Accumulator), TFT_WHITE);
+    resultBox->draw();
+}
+
+    // Handle number
+    void handleNumber(Button* button){   
+      if (Accumulator==0)
+        Accumulator= button->getText().toInt(); // First press
+      else
+        Accumulator = (Accumulator*10) + button->getText().toInt(); // Second press
+    }
+
+    // Handle =
+    void handleEquals(Button* button){
+      Num2=Accumulator;
+      switch(action){
+        default:
+        case ADD:
+          Accumulator = Num1+Num2;
+          Serial.println("+");
+          break;
+        case SUBTRACT:
+          Accumulator = Num1-Num2;
+          Serial.println("-");
+          break;  
+        case MULTIPLY:
+          Accumulator = Num1*Num2;
+          Serial.println("*");
+          break;  
+        case DIVIDE:
+          Accumulator = Num1/Num2;
+          Serial.println("/");
+          break;  
+      }
+    }
+};
+
+// Globals
+
+Calculator* calculator;
+
+// MOZ IoT
+WebThingAdapter* adapter;
+ThingPropertyValue value;
+String iotbus ("IoT-Bus");
+String accumulator, num1, num2;
+
+const char* textDisplayTypes[] = {"TextDisplay", nullptr};
+ThingDevice textDisplay("textDisplay", "IoT-Bus Calculator", textDisplayTypes);
+ThingProperty accumulatorProperty("Accumulator", "", NUMBER, nullptr);
+ThingProperty num1Property("Number 1", "", NUMBER, nullptr);
+ThingProperty num2Property(" Number 2", "", NUMBER, nullptr);
+ThingProperty functionProperty("Last Function", "", STRING, nullptr);
+
+/*
+ *  WiFi ssid and password
+ */
+const char* ssid = "NETGEAR96";
+const char* password = "phoebe1984";
+
+// Create TFT
+TFT_eSPI tft = TFT_eSPI();   
+
+// Create Touchscreen
+#define CS_PIN 16 // touch pin CS
+XPT2046_Touchscreen ts(CS_PIN, 255); // No IRQ Pin
+
+void setup() {
+  Serial.begin(115200); //Use serial monitor for debugging
+
+  Serial.println("Starting Caclulator");
+  ts.setSize(240,320); // set width, height
+  ts.begin();
+ 
+  // Turn on display
+  pinMode(33, OUTPUT); // turn on the IoT-Bus Display
+  digitalWrite(33, HIGH);
+
+    // Start WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Initialize MOZ IoT thing
+  adapter = new WebThingAdapter("textdisplayer", WiFi.localIP());
+  value.string = &iotbus;
+  accumulatorProperty.setValue(value);
+  textDisplay.addProperty(&accumulatorProperty);
+  textDisplay.addProperty(&num1Property);
+  textDisplay.addProperty(&num2Property);
+  functionProperty.setValue(value);
+  textDisplay.addProperty(&functionProperty);
+  adapter->addDevice(&textDisplay);
+  adapter->begin();
+
+  // Create and draw calculator
+  calculator = new Calculator(tft, ts);
+  calculator->draw(); 
+}
+
+String function; // needs to be around for MOZ IoT
+
+void loop() {
+  // wait for a button press
+  TS_Point p = calculator->waitTouch();
+  calculator->hitTest(p);
+
+  // update MOZ IoT 
+  value.number = calculator->getAccumulator();
+  accumulatorProperty.setValue(value);
+  value.number = calculator->getNum1();
+  num1Property.setValue(value);
+  value.number = calculator->getNum2();
+  num2Property.setValue(value); 
+  function = calculator->getFunction();
+  value.string = &function;
+  functionProperty.setValue(value);
+  adapter->update();
+  
+  delay(300);
+}
---
+++ IoT-Bus_DHT11_Thing/src/IoT-Bus_DHT11_Thing.ino
@@ -0,0 +1,198 @@
+/* 
+ *  Graphics  
+ */
+#include <Adafruit_GFX.h>
+#include "Adafruit_ILI9341.h" // Hardware-specific library
+
+#define TFT_DC 27    // Data/command pin for display
+
+#define TFT_CS 5     // CS for display
+#define TFT_MISO 19  // VSPI MISO
+#define TFT_MOSI 23  // VSPI MOSI
+#define TFT_CLK 18   // VSPI CLOCK
+#define TFT_RST 0    // no RESET pin
+
+Adafruit_ILI9341 display = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+
+/* 
+ *  MOZ IoT 
+ */
+#include <Thing.h>
+#include <WebThingAdapter.h>
+
+WebThingAdapter* adapter;
+
+const char* sensorTypes[] = {"MultiLevelSensor", nullptr};
+
+ThingDevice dht11("thermometer", "Thermometer & Hygrometer", sensorTypes);
+// ThingProperty variable_name( id, description, type, @type, label, unit, writable );  // writable not supported until 0.6
+
+ThingProperty temperature("temperature", "The temperature from x to y", NUMBER, "LevelProperty", "Temperature", "F", false);
+ThingProperty humidity("humidity", "The humidity from 0 to 100%", NUMBER, "LevelProperty", "Humidity", "percent", false);
+
+ThingPropertyValue reading;
+
+/*
+ *  WiFi ssid and password
+ */
+const char* ssid = "NETGEAR96";
+const char* password = "phoebe1984";
+
+/*
+ *  DHT11
+ */
+#include "DHTesp.h"
+
+DHTesp dht;
+
+/** Comfort profile */
+ComfortState cf;
+
+int dhtPin = 4;
+
+/**
+ * getTemperature
+ * Reads temperature from DHT11 sensor
+ * @return bool
+ *    true if temperature could be aquired
+ *    false if aquisition failed
+*/
+
+TempAndHumidity newValues;
+
+bool getTemperature() {
+	// Reading temperature for humidity takes about 250 milliseconds!
+	// Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+  newValues = dht.getTempAndHumidity();
+	// Check if any reads failed and exit early (to try again).
+	if (dht.getStatus() != 0) {
+		Serial.println("DHT11 error status: " + String(dht.getStatusString()));
+		return false;
+	}
+
+	float heatIndex = dht.computeHeatIndex(newValues.temperature, newValues.humidity);
+  float dewPoint = dht.computeDewPoint(newValues.temperature, newValues.humidity);
+  float cr = dht.getComfortRatio(cf, newValues.temperature, newValues.humidity);
+
+  String comfortStatus;
+  switch(cf) {
+    case Comfort_OK:
+      comfortStatus = "Comfort_OK";
+      break;
+    case Comfort_TooHot:
+      comfortStatus = "Comfort_TooHot";
+      break;
+    case Comfort_TooCold:
+      comfortStatus = "Comfort_TooCold";
+      break;
+    case Comfort_TooDry:
+      comfortStatus = "Comfort_TooDry";
+      break;
+    case Comfort_TooHumid:
+      comfortStatus = "Comfort_TooHumid";
+      break;
+    case Comfort_HotAndHumid:
+      comfortStatus = "Comfort_HotAndHumid";
+      break;
+    case Comfort_HotAndDry:
+      comfortStatus = "Comfort_HotAndDry";
+      break;
+    case Comfort_ColdAndHumid:
+      comfortStatus = "Comfort_ColdAndHumid";
+      break;
+    case Comfort_ColdAndDry:
+      comfortStatus = "Comfort_ColdAndDry";
+      break;
+    default:
+      comfortStatus = "Unknown:";
+      break;
+  };
+
+  Serial.println(" T:" + String(newValues.temperature) + " H:" + String(newValues.humidity) + " I:" + String(heatIndex) + " D:" + String(dewPoint) + " " + comfortStatus);
+	return true;
+}
+
+/*
+ *  displayString helper function to draw text on 
+ *  the TFT display
+ */
+const int textHeight = 8;
+const int textWidth = 6;
+const int width = 320;
+const int height = 240;
+
+String last, current; // current and last values of text
+
+void displayString(const String& str, int color) {
+  int len = str.length()+1;
+  int strWidth = len * textWidth;
+  int strHeight = textHeight;
+  int scale = width / strWidth;
+  if (strHeight > strWidth / 2) {
+    scale = height / strHeight;
+  }
+  int x = width / 2 - strWidth * scale / 2;
+  int y = height / 2 - strHeight * scale / 2;
+
+  display.setRotation(1);
+  display.setTextColor(color);
+  display.setTextSize(scale);
+  display.setCursor(x, y);
+  display.println(str);
+ }
+
+
+void setup()
+{
+  Serial.begin(115200);
+  dht.setup(dhtPin, DHTesp::DHT11);
+
+  // Turn on display
+  pinMode(33, OUTPUT); // turn on the IoT-Bus Display
+  digitalWrite(33, HIGH);
+  
+  // Start display and clear
+  display.begin();
+  display.fillScreen(ILI9341_BLACK);
+
+  // Start WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Initialize MOZ IoT thing
+  adapter = new WebThingAdapter("multilevelsensor", WiFi.localIP());
+  dht11.addProperty(&temperature);
+  //adapter->addDevice(&thermometer);
+  dht11.addProperty(&humidity);
+  adapter->addDevice(&dht11);
+  adapter->begin();
+}
+
+void loop() {
+  getTemperature();
+  current = dht.toFahrenheit(newValues.temperature);
+  if (current != last){
+    displayString(last, ILI9341_BLACK);    // clear old text by writing it black
+    displayString(current, ILI9341_WHITE); // write the new value
+    reading.number = dht.toFahrenheit(newValues.temperature);  // needs to be a PropertyValue
+    temperature.setValue(reading);         // now set the property
+    reading.number = newValues.humidity;   // needs to be a PropertyValue
+    humidity.setValue(reading);            // now set the property
+    adapter->update();                     // update the MOZ adapter
+    last = current;                        // remember the last write to be able to clear it
+   } 
+   delay(1200);
+}
